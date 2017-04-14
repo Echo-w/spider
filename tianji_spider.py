3e5b283c6767c2006fe320a039c71591f18fbf49
Echo-w <wangzihang17@163.com>
2017-04-14 21:56:13 +0800

    text3
    用<dd>标签内的<a>

--- tianji_spider.py
+++ tianji_spider.py
@@ -9,19 +9,16 @@ html = requests.get(root_url, headers = headers)
 Soup= BeautifulSoup(html.text,from_encoding='utf-8')
 
 all_a = Soup.find('div',class_='lb_box').find_all('img')
+all_a = Soup.find('div',class_='lb_box').find_all('dd')
 
 for a in all_a:
-    img_url = a['src']
-    name = img_url[-9:-4]
-    img = requests.get(img_url, headers=headers)
-    f = open(name + '.jpg', 'ab')
-    f.write(img.content)
-    f.close()    title = a.get_text()
-    href = a['href']
-    img_html=requests.get(href,headers= headers)
-    img_html_soup = BeautifulSoup(img_html.text,from_encoding='utf-8')
-    img_all = img_html_soup.find('div',class_='l_effect_img_mid').find_all('img')
+    href = a.find('a')['href']
+    title = a.get_text()
+    img_html = requests.get(href, headers=headers)
+    img_html_soup = BeautifulSoup(img_html.text, from_encoding='utf-8')
+    img_all = img_html_soup.find('div', class_='l_effect_img_mid').find_all('img')
     for b in img_all:
+        print b
         tup=b['src']
         img = requests.get(tup,headers = headers)
         name = tup[-9:-4]
